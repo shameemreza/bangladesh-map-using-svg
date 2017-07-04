5376e7391b987dfe64bc0de303ab305c2ad10b77
Shameem Reza <codechef@yahoo.com>
2017-07-04 15:02:50 +0600

    Intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bangladesh Map
+
+Bangladesh Map with sample CSS and JS added using SVG. This is just for practicing. You can fork and use it in anywhere.
+
+## Details: https://shameem.me/bangladesh-map-using-svg
---
+++ script.js
@@ -0,0 +1,28 @@
+// author: Shameem Reza
+// url: https://shameem.me
+
+$ = new jQuery.noConflict();
+$(document).ready(function(){
+	// some sample events
+	$("#division-dhaka").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Dhaka"
+	})
+	$("#division-chittagong").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Chittagong"
+	})
+	$("#division-sylhet").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Sylhet"
+	})
+	$("#division-khulna").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Khulna"
+	})
+	$("#division-barishal").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Barisal"
+	})
+	$("#division-rajshahi").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Rajshahi"
+	})
+	$("#division-rangpur").click(function(){
+		window.location.href="https://en.wikipedia.org/wiki/Rangpur_City"
+	})
+})
